96488b34e5da6b3bf5e9303b2a5fc7e1f1358b05
Faahim <abdulkaderfahim6@gmail.com>
2025-05-05 22:34:24 +0600

    BooksDetails updated

--- src/components/BooksDetails/BooksDetails.jsx
+++ src/components/BooksDetails/BooksDetails.jsx
@@ -5,11 +5,41 @@ const BooksDetails = () => {
     const data = useLoaderData();
     const Id = parseInt(bookId);
     const book = data.find(book => book.bookId === Id)
-    console.log(book)
+    const {author,bookId:cur_bookId,bookName,image,review,totalPages,rating,category,tags,yearOfPublishing,publisher}=book;
     return (
         <div>
-            <h1>Write book details.</h1>
-            {bookId}
+            <div className="flex gap-x-3" >
+                <img src={image} alt="" />
+                 <div>
+                    <h1 className="font-bold" >{bookName}</h1>
+                     <span>By: {author}</span>
+                     <p><span className="font-bold mr-1" >Review:</span>{review}</p>
+                     <p className="border-b border-gray-500 pb-2" >
+                        <span className="font-bold mr-2">tag</span>
+                        {
+                            tags.map(tag=><button className="border border-green-100 rounded-md bg-green-600 bg-opacity-65 mr-1 p-[2px]" >#{tag}</button>)
+                        }
+                     </p>
+                     <div className="flex justify-between " >
+                        <p>Number of Pages: </p>
+                         <p>{totalPages}</p>
+                     </div>
+                     <div className="flex justify-between  " >
+                        <p>Publisher: </p>
+                         <p>{publisher}</p>
+                     </div>
+                     <div className="flex justify-between  " >
+                        <p>Year of publisher: </p>
+                         <p>{yearOfPublishing}</p>
+                     </div>
+                     <div className="flex justify-between mb-3" >
+                        <p>Rating: </p>
+                         <p>{rating}</p>
+                     </div>
+                     <button className="btn btn-info mr-1" >Read</button>
+                     <button className="btn btn-success" >Wishlist</button>
+                 </div>
+            </div>
         </div>
     );
 };
